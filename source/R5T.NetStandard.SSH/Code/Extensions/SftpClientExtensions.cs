250bdd34d3723c2a5e4d6e12eda86088386c3ddd
MinexAutomation <minex.automation@gmail.com>
2019-09-10 14:44:55 -0700

    Added functionality to work-around some of the drawbacks of the SftpClient, specifically regarding directory creation.

--- source/R5T.NetStandard.SSH/Code/Extensions/SftpClientExtensions.cs
+++ source/R5T.NetStandard.SSH/Code/Extensions/SftpClientExtensions.cs
@@ -2,6 +2,8 @@
 
 using Renci.SshNet;
 
+using PathUtilities = R5T.NetStandard.IO.Paths.Utilities;
+
 
 namespace R5T.NetStandard.SSH
 {
@@ -11,6 +13,8 @@ namespace R5T.NetStandard.SSH
         /// Creates a directory (checking if the directory exists first to avoid error when the directory already exists).
         /// Can be called multiple times.
         /// The <see cref="System.IO.Directory.CreateDirectory(string)"/> method can be called multiple times without error. The <see cref="SftpClient.CreateDirectory(string)"/> method cannot. This method fixes the SFTP client behavior to be like the Directory class.
+        /// Cannot be called to make intermediate directories for nested directories. For example, if A exists, but B does not, a call to create C such that /A/B/C will fail with exception:
+        ///     Renci.SshNet.Common.SftpPathNotFoundException: 'No such file'
         /// </summary>
         public static void CreateDirectoryOkIfExists(this SftpClient sftpClient, string directoryPath)
         {
@@ -18,6 +22,27 @@ namespace R5T.NetStandard.SSH
             {
                 sftpClient.CreateDirectory(directoryPath);
             }
+        }
+
+        /// <summary>
+        /// Creates a directory, creating all required intermediate directories.
+        /// Ok if the directory already exists (idempotent) like <see cref="CreateDirectoryOkIfExists(SftpClient, string)"/>.
+        /// </summary>
+        /// <remarks>
+        /// The <see cref="Renci.SshNet.SftpClient.CreateDirectory(string)"/> method will not make intermediate directories for nested directories. For example, if A exists, but B does not, a call to create C such that /A/B/C will fail with exception:
+        ///     Renci.SshNet.Common.SftpPathNotFoundException: 'No such file'
+        /// </remarks>
+        public static void CreateDirectoryOkIfIntermediatesAndExists(this SftpClient sftpClient, string directoryPath)
+        {
+            // The SftpClient.CreateDirectory() call will not make intermediate directories as required for a path.
+            // This work-around walks up the directory tree until a parent directory exists, and makes intermediate directories as required.
+            var parentDirectoryPath = PathUtilities.GetParentDirectoryPath(directoryPath);
+            if (!sftpClient.Exists(parentDirectoryPath))
+            {
+                sftpClient.CreateDirectoryOkIfIntermediatesAndExists(parentDirectoryPath);
+            }
+
+            sftpClient.CreateDirectory(directoryPath);
         }
     }
 }
---
+++ source/R5T.NetStandard.SSH/Code/Extensions/SftpClientWrapperExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace R5T.NetStandard.SSH
+{
+    public static class SftpClientWrapperExtensions
+    {
+        /// <summary>
+        /// Creates a directory.
+        /// Idempotent, can be called multiple times (does not throw an exception if the directory already exists).
+        /// Creates all required intermediate directories for a nested directory path.
+        /// </summary>
+        public static void CreateDirectory(this SftpClientWrapper clientWrapper, string directoryPath)
+        {
+            clientWrapper.SftpClient.CreateDirectoryOkIfIntermediatesAndExists(directoryPath);
+        }
+
+        /// <summary>
+        /// Deletes a directory.
+        /// Not idempotent, cannot be called multiple times. If an attempt is made to delete a non-existent directory, "Renci.SshNet.Common.SftpPathNotFoundException: 'No such file'" is thrown.
+        /// Note, this is consistent with the <see cref="System.IO.Directory.Delete(string)"/> behavior.
+        /// </summary>
+        public static void DeleteDirectoryThrowIfNotExists(this SftpClientWrapper clientWrapper, string directoryPath)
+        {
+            clientWrapper.SftpClient.DeleteDirectory(directoryPath);
+        }
+
+        /// <summary>
+        /// Creates a directory.
+        /// Idempotent, can be called multiple times (does not throw an exception if the directory does not exist).
+        /// Note, this is different than the <see cref="System.IO.Directory.Delete(string)"/> behavior.
+        /// </summary>
+        public static void DeleteDirectory(this SftpClientWrapper clientWrapper, string directoryPath)
+        {
+            var client = clientWrapper.SftpClient;
+
+            if(client.Exists(directoryPath))
+            {
+                client.Delete(directoryPath);
+            }
+        }
+    }
+}
